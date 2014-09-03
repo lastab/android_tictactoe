c7f99ef68471dd109d250be593d8a7e78e3bd074
Batsal Awaley <batsal.awaley@gmail.com>
2014-09-03 21:59:27 +0545

    success input

--- gen/com/project/tick/R.java
+++ gen/com/project/tick/R.java
@@ -1080,14 +1080,6 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
-        public static final int Button01=0x7f05003c;
-        public static final int Button02=0x7f05003f;
-        public static final int Button03=0x7f05003e;
-        public static final int Button04=0x7f050041;
-        public static final int Button05=0x7f050040;
-        public static final int Button06=0x7f050042;
-        public static final int Button07=0x7f050043;
-        public static final int Button08=0x7f050044;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1099,11 +1091,19 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050045;
+        public static final int action_settings=0x7f050047;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
-        public static final int button1=0x7f05003d;
+        public static final int btn1=0x7f05003d;
+        public static final int btn2=0x7f05003e;
+        public static final int btn3=0x7f050042;
+        public static final int btn4=0x7f05003f;
+        public static final int btn5=0x7f050040;
+        public static final int btn6=0x7f050041;
+        public static final int btn7=0x7f050043;
+        public static final int btn8=0x7f050044;
+        public static final int btn9=0x7f050045;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1121,6 +1121,7 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int mess=0x7f05003c;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1145,6 +1146,7 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f050046;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
--- src/com/project/tick/Board.java
+++ src/com/project/tick/Board.java
@@ -4,17 +4,161 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class Board extends ActionBarActivity {
-
+	int x=0;
 	Button b1,b2,b3,b4,b5,b6,b7,b8,b9;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_board);
 		
-		b1= (Button)findViewById(R.id.Button01);
+		//initilize buttons
+		b1= (Button)findViewById(R.id.btn1);
+		b2= (Button)findViewById(R.id.btn2);
+		b3= (Button)findViewById(R.id.btn3);
+		b4= (Button)findViewById(R.id.btn4);
+		b5= (Button)findViewById(R.id.btn5);
+		b6= (Button)findViewById(R.id.btn6);
+		b7= (Button)findViewById(R.id.btn7);
+		b8= (Button)findViewById(R.id.btn8);
+		b9= (Button)findViewById(R.id.btn9);
+	
+		//add listeners
+		b1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b1.setText("X");
+				
+				else 
+					b1.setText("O");
+				x++;
+				b1.setEnabled(false);
+			}
+		});
+		b2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b2.setText("X");
+				
+				else 
+					b2.setText("O");
+				x++;
+				b2.setEnabled(false);
+			}
+		});
+		b3.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b3.setText("X");
+				
+				else 
+					b3.setText("O");
+				x++;
+				b3.setEnabled(false);
+			}
+		});
+		b4.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b4.setText("X");
+				
+				else 
+					b4.setText("O");
+				x++;
+				b4.setEnabled(false);
+			}
+		});
+		b5.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b5.setText("X");
+				
+				else 
+					b5.setText("O");
+				x++;
+				b5.setEnabled(false);
+			}
+		});
+		b6.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b6.setText("X");
+				
+				else 
+					b6.setText("O");
+				x++;
+				b6.setEnabled(false);
+			}
+		});
+		b7.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b7.setText("X");
+				
+				else 
+					b7.setText("O");
+				x++;
+				b7.setEnabled(false);
+			}
+		});
+		b8.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b8.setText("X");
+				
+				else 
+					b8.setText("O");
+				x++;
+				b8.setEnabled(false);
+			}
+		});
+		b9.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if (x%2==0)
+					b9.setText("X");
+				
+				else 
+					b9.setText("O");
+				x++;
+				b9.setEnabled(false);
+			}
+		});
+		
+	
+	}
+	public void OnClick(View view){
+		
 	}
 
 	@Override
@@ -35,4 +179,6 @@ public class Board extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
 }
